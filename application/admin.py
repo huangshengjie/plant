a90be19d237b4a823c771933219e02ec1ba00b79
huangshengjie <helloworkwork@outlook.com>
2019-01-01 17:55:36 +0800

    1

--- application/admin.py
+++ application/admin.py
@@ -1,17 +1,96 @@
 from django.contrib import admin
 from .models import *
 
+
 # Register your models here.
 
-admin.site.register(User)
-admin.site.register(Admin)
-admin.site.register(News)
-admin.site.register(Notify)
-admin.site.register(Plant)
-admin.site.register(DigitalCard)
-admin.site.register(Album)
-admin.site.register(Photo)
-admin.site.register(Judge)
-admin.site.register(Board)
-admin.site.register(Category)
-admin.site.register(PlantToCategory)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    # 每页显示10条数据
+    list_per_page = 10
+    # 控制列表页显示表的哪些字段
+    list_display = ['id', 'name', 'password', 'email', 'phone']
+    # 侧边栏过滤框
+    # list_filter = ['title', 'date', 'author']
+    # 搜索框
+    search_fields = ['name', 'email', 'phone']
+
+
+@admin.register(Admin)
+class AdminAdmin(admin.ModelAdmin):
+    list_per_page = 10
+    list_display = ['id', 'name', 'password', 'email', 'phone']
+    # list_filter = ['title', 'date', 'author']
+    search_fields = ['name', 'email', 'phone']
+
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_per_page = 10
+    list_display = ['id', 'title', 'date', 'author', 'content']
+    list_filter = ['title', 'date', 'author']
+    search_fields = ['title', 'author']
+
+
+@admin.register(Notify)
+class NotifyAdmin(admin.ModelAdmin):
+    list_per_page = 10
+    list_display = ['id', 'title', 'date', 'content']
+    list_filter = ['title', 'date']
+    search_fields = ['title', 'date']
+
+
+@admin.register(Plant)
+class PlantAdmin(admin.ModelAdmin):
+    list_per_page = 10
+    list_display = ['id', 'user_id', 'name', 'address']
+    list_filter = ['name', 'address']
+    search_fields = ['name', 'address']
+
+
+@admin.register(DigitalCard)
+class DigitalCardAdmin(admin.ModelAdmin):
+    list_per_page = 10
+    list_display = ['id', 'plant_id', 'name', 'date', 'identifier']
+    list_filter = ['name', 'date']
+    search_fields = ['name', 'date']
+
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    list_per_page = 10
+    list_display = ['id', 'plant_id', 'date', 'name', 'content']
+    list_filter = ['name', 'date']
+    search_fields = ['name', 'date']
+
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_per_page = 10
+    list_display = ['id', 'album_id', 'title', 'date', 'path']
+    list_filter = ['title', 'date']
+    search_fields = ['title', 'date']
+
+
+@admin.register(Judge)
+class JudgeAdmin(admin.ModelAdmin):
+    list_per_page = 10
+    list_display = ['id', 'plant_id', 'organizer', 'subject', 'start_date', 'end_time', 'content', 'vote']
+    list_filter = ['subject']
+    search_fields = ['subject']
+
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    list_per_page = 10
+    list_display = ['id', 'subject']
+    list_filter = ['subject']
+    search_fields = ['subject']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_per_page = 10
+    list_display = ['id', 'name']
+    list_filter = ['name']
+    search_fields = ['name']
--- application/models.py
+++ application/models.py
@@ -1,105 +1,111 @@
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
 
 # 用户表
 class User(models.Model):
-    id = models.AutoField(primary_key=True, max_length=11)
-    name = models.CharField(max_length=10)
-    password = models.CharField(max_length=16)
-    email = models.CharField(max_length=20)
-    phone = models.IntegerField(max_length=11)
+    id = models.AutoField(verbose_name='id', primary_key=True, max_length=11)
+    name = models.CharField(verbose_name='用户名', max_length=10, unique=True)
+    password = models.CharField(verbose_name='密码', max_length=16)
+    email = models.EmailField(verbose_name='邮箱', max_length=20, unique=True)
+    phone = models.IntegerField(verbose_name='手机号', unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = '用户'
 
 
 # 管理员
 class Admin(models.Model):
-    id = models.IntegerField(primary_key=True, max_length=11)
-    name = models.CharField(max_length=10)
-    password = models.CharField(max_length=16)
-    email = models.CharField(max_length=20)
-    phone = models.IntegerField(max_length=11)
+    id = models.AutoField(verbose_name='id', primary_key=True, max_length=11)
+    name = models.CharField(verbose_name='管理员名', max_length=10, unique=True)
+    password = models.CharField(verbose_name='密码', max_length=16)
+    email = models.EmailField(verbose_name='邮箱', max_length=20, unique=True)
+    phone = models.IntegerField(verbose_name='手机号', unique=True)
 
 
 # 新闻
 class News(models.Model):
-    id = models.IntegerField(primary_key=True, max_length=11)
-    title = models.CharField(max_length=25)
-    date = models.DateTimeField
-    author = models.CharField(max_length=15)
-    content = models.TextField
+    id = models.AutoField(verbose_name='id', primary_key=True, max_length=11)
+    title = models.CharField(verbose_name='标题', max_length=25)
+    date = models.DateTimeField(verbose_name='日期', default=timezone.now)
+    author = models.CharField(verbose_name='作者', max_length=15)
+    content = models.TextField(verbose_name='内容', default='')
 
 
 # 推送通知
 class Notify(models.Model):
-    id = models.IntegerField(primary_key=True, max_length=11)
-    title = models.CharField(max_length=25)
-    date = models.DateTimeField
-    content = models.TextField
+    id = models.AutoField(verbose_name='id', primary_key=True, max_length=11)
+    title = models.CharField(verbose_name='标题', max_length=25)
+    date = models.DateTimeField(verbose_name='日期', default=timezone.now)
+    content = models.TextField(verbose_name='内容', default='')
 
 
 # 植物
 class Plant(models.Model):
-    id = models.IntegerField(primary_key=True, max_length=11)
-    user_id = models.ForeignKey(to=User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=20)
-    address = models.CharField(max_length=20)
-    title = models.TextField
+    id = models.AutoField(verbose_name='id', primary_key=True, max_length=11)
+    user_id = models.ForeignKey(verbose_name='用户id', to=User, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='植物名', max_length=20)
+    address = models.CharField(verbose_name='植物产地', max_length=20)
 
 
 # 数字身份证
 class DigitalCard(models.Model):
-    id = models.IntegerField(primary_key=True, max_length=11)
-    plant_id = models.ForeignKey(to=Plant, on_delete=models.CASCADE)
-    name = models.CharField(max_length=20)
-    date = models.DateTimeField
-    identifier = models.CharField(max_length=200)
+    id = models.AutoField(verbose_name='id', primary_key=True, max_length=11)
+    plant_id = models.ForeignKey(verbose_name='植物id', to=Plant, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='名字', max_length=20)
+    date = models.DateTimeField(verbose_name='日期', default=timezone.now)
+    identifier = models.CharField(verbose_name='序列号', max_length=200)
 
 
 # 相册
 class Album(models.Model):
-    id = models.IntegerField(primary_key=True, max_length=11)
-    plant_id = models.ForeignKey(to=Plant, on_delete=models.CASCADE)
-    date = models.DateTimeField
-    name = models.CharField(max_length=20)
-    content = models.CharField(max_length=50)
+    id = models.AutoField(verbose_name='id', primary_key=True, max_length=11)
+    plant_id = models.ForeignKey(verbose_name='植物id', to=Plant, on_delete=models.CASCADE)
+    date = models.DateTimeField(verbose_name='日期', default=timezone.now)
+    name = models.CharField(verbose_name='相册名', max_length=20)
+    content = models.CharField(verbose_name='内容', max_length=50)
 
 
 # 照片
 class Photo(models.Model):
-    id = models.IntegerField(primary_key=True, max_length=11)
-    album_id = models.ForeignKey(to=Album, on_delete=models.CASCADE)
-    title = models.CharField(max_length=15)
-    date = models.DateTimeField
-    path = models.CharField(max_length=100)
+    id = models.AutoField(verbose_name='id', primary_key=True, max_length=11)
+    album_id = models.ForeignKey(verbose_name='相册id', to=Album, on_delete=models.CASCADE)
+    title = models.CharField(verbose_name='标题', max_length=15)
+    date = models.DateTimeField(verbose_name='日期', default=timezone.now)
+    path = models.CharField(verbose_name='路径', max_length=100)
 
 
 # 评判
 class Judge(models.Model):
-    id = models.IntegerField(primary_key=True, max_length=11)
-    plant_id = models.ForeignKey(to=Plant, on_delete=models.CASCADE)
-    organizer = models.IntegerField(max_length=11)
-    subject = models.CharField(max_length=20)
-    start_date = models.DateTimeField
-    end_time = models.DateTimeField
-    content = models.TextField
-    star = models.IntegerField(max_length=11)
+    id = models.AutoField(verbose_name='id', primary_key=True, max_length=11)
+    plant_id = models.ForeignKey(verbose_name='植物id', to=Plant, on_delete=models.CASCADE)
+    organizer = models.IntegerField(verbose_name='发起人')
+    subject = models.CharField(verbose_name='主题', max_length=20)
+    start_date = models.DateTimeField(verbose_name='开始时间', default=timezone.now)
+    end_time = models.DateTimeField(verbose_name='结束时间', default=timezone.now)
+    content = models.TextField(verbose_name='内容', default='')
+    vote = models.IntegerField(verbose_name='投票数', default=0)
 
 
 # 排行榜
 class Board(models.Model):
-    id = models.IntegerField(primary_key=True, max_length=11)
-    subject = models.CharField(max_length=20)
+    id = models.AutoField(verbose_name='id', primary_key=True, max_length=11)
+    subject = models.CharField(verbose_name='主题', max_length=20)
 
 
 # 类别
 class Category(models.Model):
-    id = models.IntegerField(primary_key=True, max_length=11)
-    name = models.CharField(max_length=10)
+    id = models.AutoField(verbose_name='id', primary_key=True, max_length=11)
+    name = models.CharField(verbose_name='类别名', max_length=10)
 
 
 # 中间表
 class PlantToCategory(models.Model):
-    id = models.IntegerField(primary_key=True, max_length=11)
-    plant_id = models.ForeignKey(to=Plant, on_delete=models.CASCADE)
-    category_id = models.ForeignKey(to=Category, on_delete=models.CASCADE)
+    id = models.AutoField(verbose_name='id', primary_key=True, max_length=11)
+    plant_id = models.ForeignKey(verbose_name='植物id', to=Plant, on_delete=models.CASCADE)
+    category_id = models.ForeignKey(verbose_name='类别id', to=Category, on_delete=models.CASCADE)
