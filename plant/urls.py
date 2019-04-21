62ea7d3105c5fbbee9569be1447681a26e7855f6
huangshengjie <helloworkwork@outlook.com>
2019-04-22 03:41:55 +0800

    1

--- plant/urls.py
+++ plant/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('', views.index),
     path('plant/', include('application.urls')),
     path('admins/', include('admins.urls')),
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
 ]
